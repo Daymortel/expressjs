2734bb48805ba2ba0857aaa48dd5b1010dbb5ed3
Dylan BABONNEAU <34453670+Daymortel@users.noreply.github.com>
2022-01-05 11:02:33 +0100

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const geoCode = require("../utils/geoCode");
+const stdin = process.openStdin();
+const path = require("path");
+const express = require("express");
+const axios = require("axios");
+require('dotenv').config();
+
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.set("view engine", "ejs");
+app.use(express.static(publicDirectoryPath));
+
+app.get("/", (req, res) => {
+  const city = req.params.city;
+  res.render("index", {
+    title: "Weather App",
+    name: "Dydy",
+  });
+});
+
+app.get("/temperature/:city", async (req, res) => {
+  const city = req.params.city;
+  const temperature = await geoCode(city);
+  console.log(temperature);
+  res.render("temperature", {
+    title: "Weather App",
+    temperature: `Il fait ${temperature} degrés celcius à ${city}.`
+  });
+});
+
+// app.get("/temperature/:city", (req, res) => {
+//   console.log(req.params.city);
+//   res.send("ok");
+// });
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Listenning on port ${port}...`));
